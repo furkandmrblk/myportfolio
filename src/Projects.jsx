fcf80d743e6383710ffda467231cb4f8b07a8e8e
Furkan <ibrahimdmrblk@outlook.de>
2021-01-10 18:43:08 +0100

    fixed github repo div

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -192,6 +192,7 @@ const ProjectFrameRepo = styled.a`
   margin-left: 0.7rem;
   margin-bottom: 3.75rem;
   text-decoration: none;
+  color: ${(props) => props.theme.colors.white};
   transition: all 0.15s ease-in-out;
 
   &:visited {
